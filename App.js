9e17aad46e591e2cbb38fcfed7d7103350ae460d
varshapal <35573741+varshapal@users.noreply.github.com>
2023-09-28 01:09:22 +0530

    Rendering Expenses with performance optimisation

--- App.js
+++ App.js
@@ -1,43 +1,49 @@
+import React, { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
-function App() {
 
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-    
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
   
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   
   const addExpenseHandler = expense => {
-    //console.log('In App.js');
-    //console.log(expense);
-    const displayExpense = document.getElementById('data');
-    displayExpense.textContent = JSON.stringify(expense);
-    
-    
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    })
+      
   }
+    //const displayExpense = document.getElementById('data');
+    //displayExpense.textContent = JSON.stringify(expense);
+    
+    
+  
   
   
     
@@ -60,7 +66,7 @@ function App() {
         
         <NewExpense onAddExpense = {addExpenseHandler}/>
         <Expenses items={expenses} />
-        <p id='data'>Dynamic Data</p>
+        {/* <p id='data'>Dynamic Data</p> */}
         
         
         
