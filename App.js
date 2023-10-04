2cb79729da6d5e42bb30f94a62871b0399c9e27c
varshapal <35573741+varshapal@users.noreply.github.com>
2023-10-04 16:52:30 +0530

    React Fragment & React Portals

--- App.js
+++ App.js
@@ -1,86 +1,25 @@
 import React, { useState } from 'react';
 
-import logo from './logo.svg';
-import './App.css';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
+function App() {
+  const [usersList, setUsersList] = useState([]);
 
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-  
-];
-
-const App = () => {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-  
-  const addExpenseHandler = expense => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
-    })
-      
-  }
-    //const displayExpense = document.getElementById('data');
-    //displayExpense.textContent = JSON.stringify(expense);
-    
-    
-  
-  
-  
-    
-    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-        <NewExpense onAddExpense = {addExpenseHandler}/>
-        <Expenses items={expenses} />
-        {/* <p id='data'>Dynamic Data</p> */}
-        
-        
-        
-        
-        
-        
-        
-
-        
-        
-        
-            
-      </header>
-    </div>
+    <React.Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </React.Fragment>
   );
 }
 
